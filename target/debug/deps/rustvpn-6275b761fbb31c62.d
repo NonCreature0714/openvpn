e5763403b8f3b56fae62fb09407cd6673402c55a
Bill B <brubaker.bill+git@gmail.com>
2018-12-27 02:32:05 -0600

    now, starting in Rust

---
+++ target/debug/deps/rustvpn-6275b761fbb31c62.d
@@ -0,0 +1,5 @@
+/home/pi/code/projects/openvpn/target/debug/deps/rustvpn-6275b761fbb31c62: src/main.rs
+
+/home/pi/code/projects/openvpn/target/debug/deps/rustvpn-6275b761fbb31c62.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rustvpn.d
@@ -0,0 +1,1 @@
+/home/pi/code/projects/openvpn/target/debug/rustvpn: /home/pi/code/projects/openvpn/src/main.rs
